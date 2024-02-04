73ee3f233619144b5ae3b9bbee1d5b6fbdce6635
Melted Tofu <melted@meltedtofu.com>
2024-02-03 21:28:43 -0500

    run jobs in parallel for a full collection of saves and roms
    This is a pure brute-force random walk. Once a basic navmesh is bootstrapped we can switch to a more intelligent search with that data to avoid walking on already known nodes. Will need some sort of trigger to avoid bouncing on a wall for the entire duration of a search.

--- src/PokemonCartographer.jl
+++ src/PokemonCartographer.jl
@@ -8,117 +8,107 @@ using Images
 using ProgressMeter
 using Random
 using MetaGraphsNext: labels
+using Distributed
 
 include("Nav.jl")
 using .Nav
 
-# using distributed pump out requests to run a rom with a save state for x frames and collect the results.
-# Sticking with a single computer for now, but should be easy enough to go wider when we get there
-# (local distributed for now) (figure out shared filesystem with something like sshfs)
+struct Job
+    romname::String
+    savename::String
+    duration::Int
+    emulator0::Emulator
 
-function dothething()::Nothing
-    # Given a library of roms
-    # and a collection of save states
-    # Produce a "job" to
-    #   - run a rom
-    #   - from a save state
-    #   - for x frames
-    #   - and send each back to a channel
+    Job(romname, savename, duration, emulator0) = new(romname, savename, duration, emulator0)
+end
 
-    # This is essentially a map-reduce problem
-    #   map a rom+save into a series of frames -> reduce series of frames into one stitched image per location.
+function dojob(job::Job; imageprefix::Union{String,Nothing}=nothing)::Navmesh
+    nav = Navmesh()
+    statenum = 1
+    lastpos = GameState().position
+    button = nothing
 
-    # Going one level deeper
-    #   - array of roms + saves (static for now, eventually dynamically produced)
-    #       - rom + save -> produce series of frames (every frame that is in the overworld)
-    #       - group frames by "location" (n locations x m frames)
-    #       - for each group of frames, stitch together a larger image (n locations x 1 image)
-    #   - array of (location + image)
-    #   - group by location again -> Dict{Location}{Vector{Image}}
-    #       - stitch together all frames in a location
-    #   - Dict{Location}{Image}
+    gb = deepcopy(job.emulator0)
 
-    # or in terms of types
-    # Job = Struct{Rom, Save, Duration}
-    #
-    # Vector{Jobs} -> Vector{Dict{Location, Image}} -> Dict{Location, Vector{Image}} -> Dict{Location, Image}
-    #              / \
-    #         ----     --------------------------------------------------------------------------
-    #        |                                                                                   |
-    #        Job -> Vector{Tuple{Location, Image}} -> Dict{Location, Vector{Image}} -> Dict{Location, Image}
+    for i in 1:job.duration
+        pixels = doframe!(gb)
+        game = GameState(gb, pixels)
+        buttonstate!(gb, ButtonA,     true)
+        buttonstate!(gb, ButtonB,     true)
+        buttonstate!(gb, ButtonUp,    true)
+        buttonstate!(gb, ButtonDown,  true)
+        buttonstate!(gb, ButtonLeft,  true)
+        buttonstate!(gb, ButtonRight, true)
 
-    # Maybe ignore the image idea for now. The navmesh is the thing I actually want.
-    # Sure, render the navmesh as an image, but that is a rendering step not a data step.
-    # It should be an identical type pipeline as above, just replace "image" with "navmesh".
+        if statenum == 1 # Haven't loaded the game yet keep smashing A
+            if game.position == (0x00, 0x00, 0x00) || !isnothing(game.menu)
+                buttonstate!(gb, ButtonA, i%2 == 0)
+            else
+                statenum = 2
+            end
+        elseif statenum == 2 # Unclear why this delay is needed. Just going with it for now.
+            if i < 30*60
+                buttonstate!(gb, ButtonA, i%2 == 0)
+            else
+                statenum = 3
+            end
+        elseif statenum == 3
+            if i%3 == 0
+                button = rand([ButtonUp, ButtonDown, ButtonLeft, ButtonRight])
+                buttonstate!(gb, button, false)
+            elseif lastpos != game.position &&!isnothing(button)
+                Navmesh!(nav, Position(lastpos), Position(game.position), asdirection(button))
+                lastpos = game.position
+            end
+        end
+    end
 
-    # The pipeline described above is one "batch" of work.
-    # Additional batches should be created to continue exploring until "enough" of the world has been discovered.
+    if !isnothing(imageprefix)
+        pixels = doframe!(gb)
+        save(File{format"PNG"}("$imageprefix.end.png"), reinterpret(BGRA{N0f8}, pixels))
+    end
 
+    nav
+end
+
+"""
+Create a Navmesh by playing the game.
+
+Starting with a list of roms and save states, spawn a worker for each pair and merge the resulting navmeshes.
+"""
+function dothething()::Navmesh
     roms = ["POKEMON BLUE.gb", "POKEMON RED.gb"] .|> r -> joinpath(@__DIR__, "..", "roms", r)
 
-    runduration = 10*60*60
+    saves = walkdir(joinpath(@__DIR__, "..", "saves")) .|>
+            (listing -> map(f -> joinpath(first(listing), f), last(listing))) |>
+            Base.Fix1(reduce, vcat) |>
+            Base.Fix1(filter, f -> !contains(f, "DS_Store"))
+
+    duration = 10*60*60
 
     prog = Progress(length(roms) * duration; desc="Exploring...", color=:yellow)
 
-    globe = Navmesh()
-
-    for (i, r) in enumerate(roms)
-        gb = Emulator(r)
-        nav = Navmesh()
-
-        sav = Vector{UInt8}(undef, 2^15)
-        open("$r.sav") do io
-            readbytes!(io, sav)
-        end
-        ram!(gb, sav)
-
-        statenum = 1
-        lastpos = GameState().position
-        button = nothing
-        for j in 1:duration
-            pixels = doframe!(gb)
-            game = GameState(gb, pixels)
-            buttonstate!(gb, ButtonA,     true)
-            buttonstate!(gb, ButtonB,     true)
-            buttonstate!(gb, ButtonUp,    true)
-            buttonstate!(gb, ButtonDown,  true)
-            buttonstate!(gb, ButtonLeft,  true)
-            buttonstate!(gb, ButtonRight, true)
-            #save(File{format"PNG"}("asdf.$i.$(lpad(j, 4, '0')).png"), reinterpret(BGRA{N0f8}, pixels))
-
-            if statenum == 1 # Haven't loaded the game yet keep smashing A
-                if game.position == (0x00, 0x00, 0x00) || !isnothing(game.menu)
-                    buttonstate!(gb, ButtonA, j%2 == 0)
-                else
-                    statenum = 2
-                end
-            elseif statenum == 2 # Unclear why this delay is needed. Just going with it for now.
-                if j < 30*60
-                    buttonstate!(gb, ButtonA, j%2 == 0)
-                else
-                    statenum = 3
-                end
-            elseif statenum == 3
-                if j%3 == 0
-                    button = rand([ButtonUp, ButtonDown, ButtonLeft, ButtonRight])
-                    buttonstate!(gb, button, false)
-                elseif lastpos != game.position &&!isnothing(button)
-                    Navmesh!(nav, Position(lastpos), Position(game.position), asdirection(button))
-                    lastpos = game.position
-                end
+    jobs = []
+    for r in roms
+        for s in saves
+            gb = Emulator(r)
+            sav = Vector{UInt8}(undef, 2^15)
+            open("$r.sav") do io
+                readbytes!(io, sav)
             end
+            ram!(gb, sav)
 
-            next!(prog)
+            push!(jobs, Job(r, s, duration, gb))
         end
+    end
 
-        pixels = doframe!(gb)
-        save(File{format"PNG"}("asdf.$i.end.png"), reinterpret(BGRA{N0f8}, pixels))
-        globe = Navmesh(globe, nav)
+    globe = @showprogress desc="Exploring..." color=:blue @distributed (Navmesh) for j in jobs
+        dojob(j)
     end
 
     @info length(labels(globe))
 
-    nothing
+    globe
 end
 
 end # module PokemonCartographer
